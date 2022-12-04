0be60c81da7b505288cfc85d2fc653ab027679ea
Thijs Olijerhoek <t.g.a.olijerhoek@students.uu.nl>
2022-12-04 14:16:56 +0100

    day 4 part 2 finished, clean as fuck

--- AoC-DAy4/Program.cs
+++ AoC-DAy4/Program.cs
@@ -12,25 +12,47 @@ namespace AoC_DAy4
         static void Main(string[] args)
         {
             StreamReader sr = new StreamReader("C:\\Users\\prive\\source\\repos\\niet belangrijk\\aoc1\\AoC-Day4\\TextFile1.txt");
-            Console.WriteLine("part 1: " + part1(sr));
+            //Console.WriteLine("part 1: " + part1(sr));
+            Console.WriteLine("part 2: " + part2(sr));
             Console.ReadLine();
         }
-        public static int part1(StreamReader s)
+        public static int part2(StreamReader sr)
         {
             string line;
             int count = 0;
-            while ((line = s.ReadLine()) != null)
+            while ((line = sr.ReadLine()) != null)
             {
                 string[] ranges = line.Split(',');
-                int range1Low = int.Parse(ranges[0].Split('-')[0]);
-                int range1High = int.Parse(ranges[0].Split('-')[1]);
-                int range2Low = int.Parse(ranges[1].Split('-')[0]);
-                int range2High = int.Parse(ranges[1].Split('-')[1]);
-                if ((range1Low <= range2Low && range1High >= range2High) || (range2Low <= range1Low && range2High >= range1High))
+                int r1Low = int.Parse(ranges[0].Split('-')[0]);
+                int r1High = int.Parse(ranges[0].Split('-')[1]);
+                int r2Low = int.Parse(ranges[1].Split('-')[0]);
+                int r2High = int.Parse(ranges[1].Split('-')[1]);
+                bool check1 = (r1Low <= r2Low && r2Low <= r1High);
+                bool check2 = (r2Low <= r1Low && r1Low <= r2High);
+                if ( check1 || check2)
                 {
                     count++;
                 }
-
+            }
+            return count;
+        }
+        public static int part1(StreamReader sr)
+        {
+            string line;
+            int count = 0;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] ranges = line.Split(',');
+                int r1Low = int.Parse(ranges[0].Split('-')[0]);
+                int r1High = int.Parse(ranges[0].Split('-')[1]);
+                int r2Low = int.Parse(ranges[1].Split('-')[0]);
+                int r2High = int.Parse(ranges[1].Split('-')[1]);
+                bool check1 = r1Low <= r2Low && r1High >= r2High;
+                bool check2 = r2Low <= r1Low && r2High >= r1High;
+                if (check1 || check2)
+                {
+                    count++;
+                }   
             }
             return count;
         }
